7e4d88816059a5d1233799991484cd40d039a830
Mario Roberti <mario.roberti@gmail.com>
2012-11-10 14:58:10 -0800

    Fixed the path solution parts with help from Roland Yonaba!

--- main.lua
+++ main.lua
@@ -8,7 +8,6 @@
 -- Copyright 2011 Mario Roberti. All Rights Reserved.
 -- 
 
--- Our offsets for when faking our hex map...
 debugTextGroup = display.newGroup()
 debugTextGroup.x = 100
 debugTextGroup.y = 900
@@ -16,18 +15,10 @@ local tempRect = display.newRect(-100,-25,1000,300)
 tempRect:setFillColor(0,0,0)
 debugTextGroup:insert(tempRect)
 local debugText = display.newText(debugTextGroup,"Test Debug", 100, 0, nil, 12)
--- display.CenterReferencePoint
--- display.TopLeftReferencePoint
--- display.TopCenterReferencePoint
--- display.TopRightReferencePoint
--- display.CenterRightReferencePoint
--- display.BottomRightReferencePoint
--- display.BottomCenterReferencePoint
--- display.BottomLeftReferencePoint
--- display.CenterLeftReferencePoint
 
 debugText:setReferencePoint(display.TopLeftReferencePoint)
 
+-- Our offsets for when faking our hex map...
 local yOffset = 115
 local xOffset = 98
 
@@ -56,8 +47,8 @@ gameBoard.yScale = 0.5
 -- map.
 local map = {
      {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-     {1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,1},
-     {1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,1},
+     {1,0,0,0,0,0,0,0,0,1,1,0,0,0,1,1,0,0,0,1},
+     {1,0,0,0,0,0,0,0,0,1,1,0,0,0,1,1,0,0,0,1},
      {1,0,0,1,1,1,1,0,0,1,1,0,0,1,1,1,1,0,0,1},
      {1,1,1,1,0,0,1,0,0,1,1,1,1,1,0,0,1,0,0,1},
      {1,0,0,1,0,0,1,0,0,1,1,0,0,1,0,0,1,0,0,1},
@@ -72,8 +63,8 @@ local map = {
      {1,1,1,1,0,0,1,0,0,1,1,1,1,1,0,0,1,0,0,1},
      {1,0,0,1,0,0,1,0,0,1,1,0,0,1,0,0,1,0,0,1},
      {1,0,0,1,1,1,1,0,0,1,1,0,0,1,1,1,1,0,0,1},
-     {1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,1},
-     {1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,1},
+     {1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,1,0,0,1},
+     {1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,1,0,0,1},
      {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
 }
 
@@ -90,36 +81,6 @@ gameBoard:insert(tempPlayer)
 local walkable = 1
 local allowDiagonal = false
 local pather = Jumper(map,walkable,allowDiagonal)
-local results = pather:getPath(1,1,7,7)
-local tempPath = {}
-local iteration = 1
-
-if(results~=nil)then
-	for k,v in pairs(results) do
-		local tempPoint = {}
-		tempPoint.x,tempPoint.y = v.x,v.y
-		-- Insert our x and y values into the table 'tempPath'
-		table.insert(tempPath,tempPoint)
-	end
-     
-	-- print out our path's solution coordinates
-	-- for i=1,#tempPath do
-	-- 	print("Finished steps "..tempPath[i].x,tempPath[i].y)
-	-- end
-else
-	print("Hmmm, no pathing solution found...")
-end
-
--- 
--- Main function, returns a path from [startX,startY] to [endX,endY] as an ordered array of locations ({x = ...,y = ...}). Otherwise returns nil if there is no valid path. Also returns a second value representing total cost of the move if a path was found.
--- 
--- Argument startX: The X coordinate of the starting node (positive non zero integer)
--- Argument startY: The Y coordinate of the starting node (positive non zero integer)
--- Argument endX: The X coordinate of the goal node (positive non zero integer)
--- Argument endY: The Y coordinate of the goal node (positive non zero integer)
--- Returns: a path (regular Lua table) or nil
--- Returns: the path cost (positive number) or nil
-
 
 -- OK we're going to dynamically assign the width depending on the
 -- width of each element in the array above...we'll pick 'textMap[1]'.
@@ -181,163 +142,162 @@ end
 -- make 'movingRectangle' listen for touch events
 movingRectangle:addEventListener( "touch", movingRectangle )
 
-
-local function GetRiseOverRun(source,target)
-	local rise
-	local run
-	run = source.x - target.x
-     
-	if(source.x==target.x)then
-		run = 0		
-	end
-	
-	rise = source.y - target.y
-     
-	if(source.y==target.y)then
-		rise = 0				
-	end
-	return rise,run	
-end
-
 local function SolvePath(sourceX,sourceY,targetX,targetY)
-	-- Let's make all the tiles normal color now
-	for y=1,mapHeight do
-		for x=1,mapWidth do
-			-- Make all tiles full alpha
-			-- and no colors...
-			myMap[y][x].graphic:setFillColor(255,255,255)
-		end
-	end
-	
-	local results = pather:getPath(sourceX,sourceY,targetX,targetY)
-	if(results and (sourceX~=targetX and sourceY ~= targetY) )then
-		debugText.text = ""
-		local tempPath = {}
-		local iteration = 1
-		
-		for k,v in pairs(results) do
-			local tempPoint = {}
-			tempPoint.x,tempPoint.y = v.x,v.y
-			-- Insert our x and y values into the table 'tempPath'
-			table.insert(tempPath,tempPoint)
-		end
+     -- Let's make all the tiles normal color now
+     for y=1,mapHeight do
+          for x=1,mapWidth do
+               -- Make all tiles full alpha
+               -- and no colors...
+               myMap[y][x].graphic:setFillColor(255,255,255)
+          end
+     end
+     
+     local results = pather:getPath(sourceX,sourceY,targetX,targetY)
+     if(results)then
+          debugText.text = ""
+          local tempPath = {}
+          local iteration = 1
           
-		for i=1,#tempPath do
-			local x = tempPath[i].x
-			local y = tempPath[i].y
-			tempPlayer.x,tempPlayer.y = myMap[y][x].graphic.x,myMap[y][x].graphic.y
-			myMap[y][x].graphic:setFillColor(0,200,0)
-			if(i==#tempPath)then
-				-- OK since i==#tempPath we know
-				-- we've reached the end of our path
-				-- so set that as our 'oldX' and 'oldY'
-				-- values so when the next time we click,
-				-- the path solution will START at the LAST
-				-- location we solved for. 
-				oldX = x
-				oldY = y
-			end
-		end
+          for k,v in pairs(results) do
+               local tempPoint = {}
+               tempPoint.x,tempPoint.y = v.x,v.y
+               -- Insert our x and y values into the table 'tempPath'
+               table.insert(tempPath,tempPoint)
+          end
           
-		print("tempPath "..#tempPath)
-		-- print out our path's solution coordinates
-		-- for i=1,#tempPath do
-		-- 	print("Finished steps "..tempPath[i].x,tempPath[i].y)
-		-- end
-		debugText.text = debugText.text.."Rad! Solution for path "..sourceX..","..sourceY.." "..targetX..","..targetY.." found!"
-		debugText:setReferencePoint(display.CenterLeftReferencePoint)
-	else
-		debugText.text = debugText.text.."No solution for path "..sourceX..","..sourceY.." "..targetX..","..targetY.."\n"
-		print("No solution for path "..sourceX..","..sourceY.." "..targetX..","..targetY)
-		debugText:setReferencePoint(display.CenterLeftReferencePoint)
-		-- Hmmmmm, here we should make the player
-		-- move randomly in the hopes that it will
-		-- land in another area where it CAN find a solution....
-	end
-	return tempPath
+          for i=1,#tempPath do
+               local x = tempPath[i].x
+               local y = tempPath[i].y
+               tempPlayer.x,tempPlayer.y = myMap[y][x].graphic.x,myMap[y][x].graphic.y
+               myMap[y][x].graphic:setFillColor(0,200,0)
+               if(i==#tempPath)then
+                    -- OK since i==#tempPath we know
+                    -- we've reached the end of our path
+                    -- so set that as our 'oldX' and 'oldY'
+                    -- values so when the next time we click,
+                    -- the path solution will START at the LAST
+                    -- location we solved for. 
+                    oldX = x
+                    oldY = y
+               end
+          end
+          
+          print("tempPath "..#tempPath)
+          -- print out our path's solution coordinates
+          -- for i=1,#tempPath do
+          -- 	print("Finished steps "..tempPath[i].x,tempPath[i].y)
+          -- end
+          debugText.text = debugText.text.."Rad! Solution for path "..sourceX..","..sourceY.." "..targetX..","..targetY.." found!"
+          debugText:setReferencePoint(display.CenterLeftReferencePoint)
+     else
+          debugText.text = debugText.text.."No solution for path "..sourceX..","..sourceY.." "..targetX..","..targetY.."\n"
+          print("No solution for path "..sourceX..","..sourceY.." "..targetX..","..targetY)
+          debugText:setReferencePoint(display.CenterLeftReferencePoint)
+          -- Hmmmmm, here we should make the player
+          -- move randomly in the hopes that it will
+          -- land in another area where it CAN find a solution....
+     end
+     return results
 end
 
 -- Our event handler for clicking our tiles...
 local function ClickHandler(event) 
-	if(event.phase=="ended")then
-		tempPlayer.x = oldX * xOffset
-		tempPlayer.y = oldY * yOffset
-		tempPlayer:toFront()
-		print("Clicked on "..event.target.name)
-		-- OK, we're going to parse the results
-		-- '3,12' or '10,4' and split it on the comma
-		-- so we can get an x and y coordinate
-		local tempResults = SolvePath(oldX,oldY,event.target.col,event.target.row)
-	end
-	return true 
+     if(event.phase=="ended")then
+          print("Clicked on "..event.target.name)
+          local tempResults = SolvePath(oldX,oldY,event.target.col,event.target.row)
+          if(tempResults)then
+               print("Success?")
+               tempPlayer.x = oldX * xOffset
+               tempPlayer.y = oldY * yOffset
+               tempPlayer:toFront()          
+          else
+               local tempResults = SolvePath(oldX,oldY,oldX,oldY)
+          end
+     end
+     return true 
 end 
 
+function CreateGraphicMap(passedMap)
+     local myMap = {}
+     for y=1,mapHeight do
+          myMap[y] = {}     -- create a new row
+          for x=1,mapWidth do
+               -- OK, we're making our actual object a table that we 
+               -- can throw goodies in later on...
+               myMap[y][x] = {}
+               -- We'll assign the variable 'value' a text value so we can parse
+               -- it later on
+               
+               local scale = 6.50
+               
+               local fileName = ""
+               local tempValue = passedMap[y][x] -- This will go to the 'xth' place in the string and give us the character at 'x'
+               if(tempValue==0)then
+                    myMap[y][x].value = "0"
+                    fileName="wall.jpg"
+                    myMap[y][x].graphic = display.newImage(fileName)
+               elseif(tempValue==1) then
+                    myMap[y][x].value = "1"	
+                    fileName="floor.jpg"
+                    myMap[y][x].graphic = display.newImage(fileName)
+               else
+                    -- myMap[y][x].value = "2"	
+                    -- fileName="desert.png"
+               end		
+               -- We'll assign the variable 'graphic' a newImage value
+               -- and we're loading from the fileName selected above
+               -- depending on which random number we picked
+               
+               -- Let's name the graphic to return a name when
+               -- it's clicked!
+               myMap[y][x].graphic.name = x..","..y
+               -- Let's add some new values to make life
+               -- easier when doing pathfinding:
+               myMap[y][x].graphic.col = x
+               myMap[y][x].graphic.row = y
+               -- Add event handler to the graphic
+               myMap[y][x].graphic:addEventListener("touch", ClickHandler )
+               -- NEW! We're inserting the tiles in our parent display group
+               -- called 'gameBoard'
+               gameBoard:insert(myMap[y][x].graphic)
+               -- Handle the offset by using modulo to determine
+               -- if we're on an even or odd column, and adjust the
+               -- spacing accordingly
+               --		if math.mod(x, 2) == 0 then
+               --			--print("even")
+               --			myMap[y][x].graphic.x = x*xOffset
+               --			myMap[y][x].graphic.y = (y * yOffset)
+               --		else
+               --			--print("odd")
+               --			myMap[y][x].graphic.x = x*xOffset
+               --			myMap[y][x].graphic.y = (y * yOffset) - (yOffset / 2)
+               --		end
+               myMap[y][x].graphic.x = x*xOffset
+               myMap[y][x].graphic.y = (y * yOffset)
+               
+               -- For debugging, let's add a text with coordinates
+               local tempText = display.newText(gameBoard,myMap[y][x].graphic.name, myMap[y][x].graphic.x-40, myMap[y][x].graphic.y-12, nil, 32)
+               
+          end
+     end
+     return myMap
+end
 -- Make the 'myMap' table so we can
 -- Duplicate the existing map into it,
 -- but also add bonus stuff like graphics
 -- touch events, other values, etc.
-myMap = {}
-for y=1,mapHeight do
-	myMap[y] = {}     -- create a new row
-	for x=1,mapWidth do
-		-- OK, we're making our actual object a table that we 
-		-- can throw goodies in later on...
-		myMap[y][x] = {}
-		-- We'll assign the variable 'value' a text value so we can parse
-		-- it later on
-               
-		local scale = 6.50
-		
-		local fileName = ""
-		local tempValue = map[y][x] -- This will go to the 'xth' place in the string and give us the character at 'x'
-		if(tempValue==0)then
-			myMap[y][x].value = "0"
-			fileName="wall.jpg"
-			myMap[y][x].graphic = display.newImage(fileName)
-		elseif(tempValue==1) then
-			myMap[y][x].value = "1"	
-			fileName="floor.jpg"
-			myMap[y][x].graphic = display.newImage(fileName)
-		else
-			-- myMap[y][x].value = "2"	
-			-- fileName="desert.png"
-		end		
-		-- We'll assign the variable 'graphic' a newImage value
-		-- and we're loading from the fileName selected above
-		-- depending on which random number we picked
-          
-		-- Let's name the graphic to return a name when
-		-- it's clicked!
-		myMap[y][x].graphic.name = x..","..y
-		-- Let's add some new values to make life
-		-- easier when doing pathfinding:
-		myMap[y][x].graphic.col = x
-		myMap[y][x].graphic.row = y
-		-- Add event handler to the graphic
-		myMap[y][x].graphic:addEventListener("touch", ClickHandler )
-		-- NEW! We're inserting the tiles in our parent display group
-		-- called 'gameBoard'
-		gameBoard:insert(myMap[y][x].graphic)
-		-- Handle the offset by using modulo to determine
-		-- if we're on an even or odd column, and adjust the
-		-- spacing accordingly
-          --		if math.mod(x, 2) == 0 then
-          --			--print("even")
-          --			myMap[y][x].graphic.x = x*xOffset
-          --			myMap[y][x].graphic.y = (y * yOffset)
-          --		else
-          --			--print("odd")
-          --			myMap[y][x].graphic.x = x*xOffset
-          --			myMap[y][x].graphic.y = (y * yOffset) - (yOffset / 2)
-          --		end
-          myMap[y][x].graphic.x = x*xOffset
-          myMap[y][x].graphic.y = (y * yOffset)
-          
-		-- For debugging, let's add a text with coordinates
-		local tempText = display.newText(gameBoard,myMap[y][x].graphic.name, myMap[y][x].graphic.x-40, myMap[y][x].graphic.y-12, nil, 32)
-		
-	end
+myMap = CreateGraphicMap(map)
+
+
+local function ClearMap(passedMap)
+     passedMap = {}
+     for y=1,mapHeight do
+          passedMap[y] = {}     -- create a new row
+          for x=1,mapWidth do
+               display.remove(passedMap[y][x].graphic)
+               myMap[y][x].graphic = nil
+          end
+     end
 end
 
 debugTextGroup:toFront()
-
